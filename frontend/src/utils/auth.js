1926c65707f2650b690eb6d20420de37e057658f
Klimanova Julia <sempppai124@yandex.ru>
2023-07-30 01:39:51 +0300

    вставила доменные ссылки

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,5 +1,6 @@
 //export const BASE_URL = 'https://auth.nomoreparties.co';
-export const BASE_URL = 'http://localhost:3000';
+//export const BASE_URL = 'http://localhost:3000';
+export const BASE_URL = 'http://sempusha.nomoreparties.co';
 
 function checkResponse(res) {
   if (res.ok) {
